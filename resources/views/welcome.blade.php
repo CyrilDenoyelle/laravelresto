b51b1e3907154fc2f65923d673728db6533878bf
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2018-02-19 14:09:16 +0100

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    </head>
+    <body>
+    <div class="container">
+                @auth
+                    <a class="btn btn-success btn-lg btn-block" role="button" href="{{ url('/commande') }}" >Commander</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+                    <a href="{{ route('register') }}">Register</a>
+                @endauth</div>
+    </body>
+</html>
