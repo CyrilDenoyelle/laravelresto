9d21ab6ddc2d463a5faadaba4625d077c133a71b
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2018-02-19 14:59:45 +0100

    ajout de style css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,26 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     </head>
     <body>
-    <div class="container">
-        @auth
-            <a class="btn btn-success btn-lg btn-block" role="button" href="{{ url('/commande') }}" >Commander</a>
-        @else
-            <a href="{{ route('login') }}">Login</a>
-            <a href="{{ route('register') }}">Register</a>
-        @endauth</div>
+
+        <div class="container">
+            @auth
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+
+                <a class="btn btn-success btn-lg btn-block" role="button" href="{{ url('/commande') }}" >Commencer une commande</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+            @endauth
+        </div>
     </body>
 </html>
